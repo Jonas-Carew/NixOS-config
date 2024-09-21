44f99b8e0ad1a7b84cb93c912bcafe8d89c21cf4
jonasc <jonasc@omonporch@nixos>
2024-09-20 21:33:14 -0700

    added firacode module

--- hosts/omon/home.nix
+++ hosts/omon/home.nix
@@ -1,8 +1,14 @@
 { inputs, pkgs, lib, username, ... }: {
 
 	imports = [
+		# neovim with config
 		(import ../../modules/neovim)
+
+		# tmux with config
 		(import ../../modules/tmux)
+
+		# use FiraCode nerdfont w/ symbols
+		(import ../../modules/firacode)
 	];
 
 	# Home manager config
@@ -54,48 +60,7 @@
 
 		fzf = { enable = true; };
 
-		jq = { enable = true; };
-
-		/*tmux = {
-			enable = true;
-			extraConfig = ''
-				# set command bind to Ctrl-a instead of Ctrl-b
-				unbind C-b
-				set-option -g prefix C-a
-				bind-key C-a send-prefix
-
-				# change window splitting keybinds
-				bind | split-window -h -c "#{pane_current_path}"
-				bind - split-window -v -c "#{pane_current_path)"
-				unbind '"'
-				unbind %
-
-				# easier movement
-				bind -n M-Left select-pane -L
-				bind -n M-Right select-pane -R
-				bind -n M-Up select-pane -U
-				bind -n M-Down select-pane -D
-
-				# enable mouse
-				set -g mouse on
-				# auto-renaming off
-				set-option -g allow-rename off
-
-				# index at 1
-				set -g base-index 1
-				set -g pane-base-index 1
-				set-window-option -g pane-base-index 1
-				set-option -g renumber-windows on
-
-				# prefix highlighting
-				set -g status-left '#{?client_prefix,#[reverse]<Prefix>#[noreverse] ,}[#S] '
-			'';
-			plugins = with pkgs.tmuxPlugins; [
-				sensible
-				resurrect
-				gruvbox
-			];
-		};*/
+		#jq = { enable = true; };
 
 		git = {
 			enable = true;
