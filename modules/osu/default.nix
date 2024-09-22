a117821fc56d2c91b44f9df8d6514958418fb2dd
jonasc <jonasc@omonporch@nixos>
2024-09-22 16:37:04 -0700

    added osutransfer function

--- modules/osu/default.nix
+++ modules/osu/default.nix
@@ -6,6 +6,11 @@
 					ssh carewj@flip1.engr.oregonstate.edu
 				'';
 			};
+			osutransfer = {
+				body = ''
+					scp $argv carewj@flip1.engr.oregonstate.edu:~/transfer
+				'';
+			};
 		};
 	};
 }
